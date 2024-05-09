8dcf2f452e2b29a2fc8e318c99f0303118e3cdc3
Your Name <you@example.com>
2024-05-09 00:16:00 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -169,7 +169,13 @@ Pair * upperBound(TreeMap * tree, void* key) {
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
-    return NULL;
+    if (tree == NULL || tree->root == NULL) return NULL;
+    TreeNode *aux = tree->root;
+    while (aux->left != NULL)
+    {
+        aux = aux->left;
+    }
+    return aux->pair;
 }
 
 Pair * nextTreeMap(TreeMap * tree) {
